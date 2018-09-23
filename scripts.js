ed36d21bbb2fdddb7a791aec8dd4c4ffcc26621e
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-09-23 16:37:42 +0300

    Testing the incoming data from API if its working

--- scripts.js
+++ scripts.js
@@ -2,10 +2,16 @@
 var request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', ture);
+request.open('GET', 'https://ghibliapi.herokuapp.com/films',true);
 
 request.onload = function () {
-    // Begin accessing JSON data here   
+    // Begin accessing JSON data here   '
+    var data = JSON.parse(this.response);
+
+    data.forEach(movie => {
+        // Log each movie's title
+        console.log(movie.title);
+    })
 }
 
 // Send request
